4685bca520cb8c8239ae7f1b0163ef4bf995318e
Mateus Lopes <devmateuslopes@gmail.com>
2024-07-16 01:56:34 -0300

    style: page swagger

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -16,7 +16,11 @@ const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger
 const app = express();
 app.use(express.json())
 app.use(cors());
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile, { customCssUrl: CSS_URL }));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile, {
+  customCss:
+    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  customCssUrl: CSS_URL
+}));
 
 app.use(router)
 
